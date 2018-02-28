262ce18a7f40a30ede08b7e4c32fc85dfe4fc409
Danny Chan <dcyc88@hotmail.com>
2018-03-01 02:03:32 +0800

    simple cipher

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,23 +1,38 @@
-var Cipher = function (input) {
-
-    this.cipher = input;
-  
-    if (!input.match(/^[a-z]+$/g)) {
-         throw new Error('Only lowercase letters accepted');    
-    } 
-
+function Cipher(key) {
+    
+    if (key === '') {
+        throw('Bad key');
+    }
+    if (!(/^[a-z]+$/g).test(key)) {
+         throw ('Only lowercase letters accepted');    
+    }
 };
    
 
-Cipher.prototype.encode = function (input) {    
+Cipher.prototype.encode = function (key) {    
+    var result = []; 
+    for (var x in key) {
+            if (key[x] == 'c') {
+                result.push('g');
+            } else if (key[x] == 'g') {
+                result.push('c');
+            } else if (key[x] == 't') {
+                result.push('a');
+            } else if (key[x] == 'a') {
+                result.push('u');
+            }  else {
+                throw new Error('Invalid input')
+                break;
+            }
+    }
+    Cipher.key = result.join('');
+    return Cipher.key;
+};
+
+Cipher.prototype.decode = function () {    
+
 
     
 };
 
-Cipher.prototype.decode = function (input) {    
-
-
-    
-};
-
-module.exports = DnaTranscriber;
+module.exports = Cipher;

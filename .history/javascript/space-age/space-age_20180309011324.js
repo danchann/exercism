cd4e981ea2801cc1ff93b00a7ddcf9f2b00c5f43
Danny Chan <dcyc88@gmail.com>
2018-03-09 01:15:19 +0800

    ..

---
+++ .history/javascript/space-age/space-age_20180309011324.js
@@ -0,0 +1,31 @@
+function SpaceAge(val1, val2) {
+    this.val1 = val1;
+    this.val2 = val2
+};
+
+SpaceAge.prototype.onEarth = function() {
+    var age = (this.val / 31557600).toFixed(2);
+}
+SpaceAge.prototype.onMercury = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/0.2408467;
+}
+SpaceAge.prototype.onVenus = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/0.61519726;
+}
+SpaceAge.prototype.onMars = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/1.8808158;
+}
+SpaceAge.prototype.onJupiter = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/11.862615;
+}
+SpaceAge.prototype.onSaturn = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/29.447498;
+}
+SpaceAge.prototype.onUranus = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/84.016846;
+}
+SpaceAge.prototype.onNeptune = function() {
+    var age = (this.seconds / 31557600).toFixed(2)/164.79132;
+}
+
+module.exports = SpaceAge;
--- .history/javascript/space-age/space-age_20180309011325.js
+++ .history/javascript/space-age/space-age_20180309011325.js
@@ -7,25 +7,25 @@ SpaceAge.prototype.onEarth = function() {
     var age = (this.val1 / 31557600).toFixed(2);
 }
 SpaceAge.prototype.onMercury = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/0.2408467;
+    var age = (this.seconds / 31557600).toFixed(2)/0.2408467;
 }
 SpaceAge.prototype.onVenus = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/0.61519726;
+    var age = (this.seconds / 31557600).toFixed(2)/0.61519726;
 }
 SpaceAge.prototype.onMars = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/1.8808158;
+    var age = (this.seconds / 31557600).toFixed(2)/1.8808158;
 }
 SpaceAge.prototype.onJupiter = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/11.862615;
+    var age = (this.seconds / 31557600).toFixed(2)/11.862615;
 }
 SpaceAge.prototype.onSaturn = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/29.447498;
+    var age = (this.seconds / 31557600).toFixed(2)/29.447498;
 }
 SpaceAge.prototype.onUranus = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/84.016846;
+    var age = (this.seconds / 31557600).toFixed(2)/84.016846;
 }
 SpaceAge.prototype.onNeptune = function() {
-    var age = (this.val2 / 31557600).toFixed(2)/164.79132;
+    var age = (this.seconds / 31557600).toFixed(2)/164.79132;
 }
 
 module.exports = SpaceAge;

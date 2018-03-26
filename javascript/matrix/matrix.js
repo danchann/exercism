609ff8b907ef63a2da8dc5c0db28d0353ef54733
Danny Chan <dcyc88@hotmail.com>
2018-03-26 14:41:54 +0800

    finished matrix

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,22 +1,25 @@
-function Matrix (str) {
+module.exports = function Matrix (str) {
 
     this.columns = [];
     this.rows = [];
 
     arr = str.split('\n');
-
+    // get rows
     for (var x in arr) {
-        this.rows.push(arr[x].split(' '))
+        this.rows.push(arr[x].split(' ').map(Number))
     }
-
-    // get column
-    for (y=0; y<this.rows.length; y++) {
+    // get column and turn into arrays
+    var x=0;
+    for (var y=0; y<this.rows[x].length; y++) {
         this.columns[y] = []
-        this.columns[y].push(row[y])
+        for (x=0; x<this.rows.length; x++) {
+            this.columns[y].push(this.rows[x][y])
+        }
+        x=0;    
     }
-
     }
 
 //module.exports = Matrix;
-
-new Matrix('89 1903 3\n18 3 1\n9 4 800').columns[1]
+//new Matrix('1 2\n10 20').rows[0]
+//new Matrix('9 8 7\n19 18 17').rows[1]
+//new Matrix('89 1903 3\n18 3 1\n9 4 800').columns[1]

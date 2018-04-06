dbf273db22c2824d5887834b4d3eb70c50f45cd6
Danny Chan <dcyc88@gmail.com>
2018-04-06 08:41:12 +0800

    ..

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,20 +1,22 @@
-var Robot = function() { 
-    this.name = genName();
+let Robot = function(name) { 
+    this.rName = name;
+    //this.rName = //{ name: [] };
+    this.name = genName(name);
     //console.log(counter, this.name, resets);
 }
 
 Robot.prototype.reset = function() {
 	var oldName = this.name;
-    this.name = genName();
+    this.name = genName(oldName);
     //console.log(this.name, this.name == robot.name)
     resets++
 };
 var loop=0;
 var resets = 0;
 var counter = 0;    
-var robot = { name: [] };
+//var robot = { name: [ ] };
 
-function genName() {
+function genName(name, oldName) {
     var result = false;
     Lett1 = String.fromCharCode(capLet()); 
     Lett2 = String.fromCharCode(capLet()); 
@@ -22,7 +24,13 @@ function genName() {
     
     randName = String(Lett1+Lett2 + Numb)
     while (result==false || loop == 11000) {
-        robot.name.includes(randName) ? (randName = genName()) : (robot.name.push(randName), result=true); 
+        this.rName == undefined ? this.rName={ name: [] } : "";
+        if (this.rName.name.includes(randName)) {
+            (randName = genName());
+        } else {
+            (this.rName.name.push([randName]), result=true);
+        }
+          
         loop++;      
     }
     counter++;
@@ -41,21 +49,26 @@ function randNum() {
     return number;
 }
 
-module.exports = Robot;
-// robot = new Robot();
+// module.exports = Robot;
+// let robot = new Robot();
 // robot.reset();
 
-//Robot()
-// function lots() {
-// var i;
-//     var numRobots = 10000;
-//     var usedNames = {};
+// Robot()
+function lots() {
+var i;
+    var numRobots = 10000;
+    var usedNames = {};
+    var prev = 0, fault = 0;
 
-//     for (i = 0; i < numRobots; i++) {
-//       var newRobot = new Robot();
-//       usedNames[newRobot.name] = true;
-//     }
-//     console.log(i)
-// }
+    for (i = 0; i < numRobots; i++) {
+      var newRobot = new Robot();
+      usedNames[newRobot.name] = true;
+      prev == Object.keys(usedNames).length -1 ? "" :
+      fault++, console.log(prev+" , "+Object.keys(usedNames).length+"Fault: "+fault);
+      
+      prev = Object.keys(usedNames).length;
+    }
+    console.log("Counter: "+counter + " Length: " + Object.keys(usedNames).length)
+}
 
-// lots()
+lots()

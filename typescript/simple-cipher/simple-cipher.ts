ecd408fae230746058855d3cc7ba10027732619e
Dan Chan <dcyc88@gmail.com>
2018-05-22 07:41:43 +0800

    revised simple-cipher

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -2,55 +2,50 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 class SimpleCipher {
     key: string
 
-    constructor(key?: string) {
+    constructor(key ? : string) {
         if (key === '' || !(/^[a-z]+$/g).test(key!)) {
-             throw new Error('Bad key');    
+            throw new Error('Bad key');
         }
         this.key = key || generateRandomKey();
     }
 
-    encode( decodeMessage: string): string {
+    encode(decodeMessage: string): string {
         let encodeMessage = ''
         let arr = decodeMessage.split('')
-        let results =[];
-        let x: any
-        for (x in arr) {
-            let keyIndex = x % this.key.length;
-            let newIndex = alphabet.indexOf(arr[x]) + 
-        alphabet.indexOf(this.key[keyIndex]);
+        let results = [];
+        for (let x in arr) {
+            let keyIndex = Number(x) % this.key.length;
+            let newIndex = alphabet.indexOf(arr[x]) +
+                alphabet.indexOf(this.key[keyIndex]);
             if (newIndex >= alphabet.length) {
-                newIndex -= alphabet.length; 
+                newIndex -= alphabet.length;
             }
-              results.push(alphabet[newIndex]);
-            }    
-            encodeMessage = results.join('')   
-            return encodeMessage
+            results.push(alphabet[newIndex]);
+        }
+        encodeMessage = results.join('')
+        return encodeMessage
     }
 
-    decode( simpleCipher: any ): string {
+    decode(simpleCipher: string): string {
         let decodeMessage = ''
         let arr = simpleCipher.split('')
-        let results =[];
-        let x: any
-        for (x in simpleCipher) {
+        let results = [];
+        for (let x = 0; x < simpleCipher.length; x++) {
             var newIndex = alphabet.indexOf(arr[x]) - alphabet.indexOf(this.key[x])
             if (newIndex < 0) {
-                newIndex += alphabet.length; 
+                newIndex += alphabet.length;
             }
-              results.push(alphabet[newIndex]);
-            }       
-            decodeMessage = results.join('')
-            return decodeMessage
-
+            results.push(alphabet[newIndex]);
+        }
+        decodeMessage = results.join('')
+        return decodeMessage
     }
 }
 
 function generateRandomKey() {
-    
-    var i;
-    var randomKey = '';
-    for (i=0; i < 100; i++) {
-        var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    let randomKey = '';
+    for (let i = 0; i < 100; i++) {
+        let getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
         randomKey += getRandomLetter;
     }
     return randomKey;
